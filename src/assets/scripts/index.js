1aa3b0a9251c90d1d62ec591c67efd030ba9ddf2
FragRappy <breton.charles@live.fr>
2023-10-06 21:45:16 +0200

    modification comportement burger et ajout style text hero

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,17 +1,18 @@
 // Gestion du scroll pour afficher le menu en fixed
 import { scrollPosition } from "./specific/scroll";
-window.addEventListener("scroll", scrollPosition.headerPosition);
+window.addEventListener('scroll', (scrollPosition.headerPosition))
 
 //Gestion des cards pour mes différents emplois
 import { emplois } from "./data/experiences";
 
 //Gestion en temps réel du menu en mode mobile et tablet/desktop
-window.addEventListener("DOMContentLoaded", (event) => {
+window.addEventListener('DOMContentLoaded', (event) => {
     const menu = document.querySelector('.menu__nav');
     const menuToogle = document.querySelector('.menu__toogle');
 
     menuToogle.addEventListener('click', () => {
         menu.style.display = (menu.style.display == 'none')? '':'none';
+        menu.classList.toggle('menu__nav__on');
     });
 
      window.onload = () => {
--- src/assets/scripts/specific/scroll.js
+++ src/assets/scripts/specific/scroll.js
@@ -2,6 +2,6 @@ export const scrollPosition = {
     headerPosition: () => {
         const lastScrollCheck = Math.round(window.scrollY);
         const menuFix = document.querySelector('.menu');
-        lastScrollCheck > 150? menuFix.classList.add('menu__fixed') : menuFix.classList.remove('menu__fixed');
+        lastScrollCheck > 100? menuFix.classList.add('menu__fixed') : menuFix.classList.remove('menu__fixed');
     }
 };
