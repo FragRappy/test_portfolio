8278e01bf49c60e51e99728384929f7a82579e27
FragRappy <breton.charles@live.fr>
2023-10-07 03:11:02 +0200

    preparation pour l'insertion des cards portfolio et divers correctifs

---
+++ src/assets/scripts/data/portfolio.js
@@ -0,0 +1,8 @@
+export default {
+    id1: {
+        Type: 'Site web',
+        Desc: 'test',
+        url: new URL('https://s02-atelier-revision-bonus-frag-rappy-yg3wgz85f.vercel.app'),
+        thumbImg: ('../../assets/img/thumb/timups.png'),
+    }
+}
--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -5,26 +5,36 @@ window.addEventListener('scroll', (scrollPosition.headerPosition))
 //Gestion des cards pour mes différents emplois
 import { emplois } from "./data/experiences";
 
-//Gestion en temps réel du menu en mode mobile et tablet/desktop
+//gestion des cards de portfolio
+import portfolios from "./data/portfolio";
+
+//Gestion en temps réel des menus et boutons en mode mobile et tablet/desktop
 window.addEventListener('DOMContentLoaded', (event) => {
     const menu = document.querySelector('.menu__nav');
     const menuToogle = document.querySelector('.menu__toogle');
-
+    const totop = document.querySelector('.totop');
+    const theme = document.querySelector('.menu__theme');
+    //gestion du clic sur le menu burger
     menuToogle.addEventListener('click', () => {
         menu.style.display = (menu.style.display == 'none')? '':'none';
         menu.classList.toggle('menu__nav__on');
     });
+    //gestion du clic sur le bouton de changement de theme
 
+    //gestion du clic sur le bouton retourner en haut
+    totop.addEventListener('click', () => {
+        window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+    });
+    //gestion de la largeur au chargement de la page
      window.onload = () => {
         const width = window.innerWidth;
-        menu.style.display = ( width > 769 )? '':'none';
-        menuToogle.style.display = ( width > 769 )? 'none':'';
+        menu.style.display = ( width > 899 )? '':'none';
+        menuToogle.style.display = ( width > 899 )? 'none':'';
     };
-
+    //gestion de la largeur au changement de taille de la fenêtre
     window.onresize = () => {
         const width = window.innerWidth;
-        menu.style.display = ( width > 769 )? '':'none';
-        menuToogle.style.display = ( width > 769 )? 'none':'';
+        menu.style.display = ( width > 899 )? '':'none';
+        menuToogle.style.display = ( width > 899 )? 'none':'';
     };
 });
-
