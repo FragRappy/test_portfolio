dd19b58fea1fcc1d49993dae5a05f79ba281f07b
FragRappy <breton.charles@live.fr>
2023-10-07 03:38:26 +0200

    test2

--- src/assets/scripts/specific/scroll.js
+++ src/assets/scripts/specific/scroll.js
@@ -3,7 +3,7 @@ export const scrollPosition = {
         const lastScrollCheck = Math.round(window.scrollY);
         const menuFix = document.querySelector('.menu');
         const totop = document.querySelector('.totop');
-        lastScrollCheck > 0 || lastScrollCheck < 0? menuFix.classList.add('menu__fixed') : menuFix.classList.remove('menu__fixed');
+        lastScrollCheck > 100 ? menuFix.classList.add('menu__fixed') : menuFix.classList.remove('menu__fixed');
         lastScrollCheck != 0? totop.style.display ='flex' : totop.style.display ='none';
     }
 };
